6fdb8781e7b130bb2417ef2ccb8f4e4a7f359ede
David ogola <Davidogola10@gmail.com>
2024-03-22 15:38:14 -0700

    Install a package

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,8 @@
+# install flask using Puppet
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+}
+
