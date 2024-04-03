adeed7673dfeb33282b79c6f4b4ef6c9aafef24d
David ogola <Davidogola10@gmail.com>
2024-04-03 05:42:09 -0700

    web servers

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,19 @@
-#!/usr/bin/env bash
-# Install NginX
-# With puppet
-
-# Update package lists
-exec { 'apt-get-update':
-  command => '/usr/bin/apt-get update',
-}
-
-# Install Nginx package
+# Install Nginx web server (w/ Puppet)
 package { 'nginx':
-  ensure  => installed,
-  require => Exec['apt-get-update'],
+  ensure => installed,
 }
 
-# Create index.html file
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
 file { '/var/www/html/index.html':
   content => 'Hello World!',
-  require => Package['nginx'],
 }
 
-exec {'redirect_me':
-  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-  provider => 'shell'
-}
-
-# Ensure Nginx service is running and restart when configuration changes
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
